b52dbddded0c5d2447a9e4afec65dbcff15db438
Toby <tobyeclark@gmail.com>
2024-02-26 17:42:36 +0000

    added title text to picture and added menu buttons that highlight when hovered

--- Menu.java
+++ Menu.java
@@ -2,13 +2,17 @@ import javax.swing.JFrame;
 
 public class Menu {
     GameArena menu = new GameArena(1000, 1000);
-    Cat cat = new Cat(0, 0);
+    Text playText = new Text("PLAY", 50, 5, 700, "WHITE");
+    Text levelsText = new Text("LEVELS", 50, 5, 750, "WHITE");
+    Text exitText = new Text("EXIT", 50, 5, 800, "WHITE");
     
     public Menu()
     {
         menu.setBackgroundImage("cat.jpeg");
         menu.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        cat.addTo(menu);
+        menu.addText(playText);
+        menu.addText(levelsText);
+        menu.addText(exitText);
     }
     
     public void quit()
@@ -20,11 +24,28 @@ public class Menu {
     {
         while(true)
         {
-            cat.update();
+            System.out.println(menu.getMousePositionX());
             menu.pause();
-            if(menu.leftMousePressed())
+            if(menu.getMousePositionX() > 5 && menu.getMousePositionX() < 137 && menu.getMousePositionY() > 650 && menu.getMousePositionY() < 700)
             {
-                break;
+                playText.setColour("RED");
+                levelsText.setColour("WHITE");
+                exitText.setColour("WHITE");
+            }else if(menu.getMousePositionX() > 5 && menu.getMousePositionX() < 1957 && menu.getMousePositionY() > 700 && menu.getMousePositionY() < 750)
+            {
+                playText.setColour("WHITE");
+                levelsText.setColour("RED");
+                exitText.setColour("WHITE");
+            }else if(menu.getMousePositionX() > 5 && menu.getMousePositionX() < 118 && menu.getMousePositionY() > 750 && menu.getMousePositionY() < 800)
+            {
+                playText.setColour("WHITE");
+                levelsText.setColour("WHITE");
+                exitText.setColour("RED");
+            }else
+            {
+                playText.setColour("WHITE");
+                levelsText.setColour("WHITE");
+                exitText.setColour("WHITe");
             }
         }
     }
