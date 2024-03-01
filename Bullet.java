d971e4bd1352d51f7e0a5649c96fc11ce53091c9
Toby <tobyeclark@gmail.com>
2024-03-01 18:18:14 +0000

    Bullet now inherits from GameObject

--- Bullet.java
+++ Bullet.java
@@ -1,16 +1,12 @@
-public class Bullet {
-    private double x;
-    private double y;
+public class Bullet extends GameObject{
     private int size = 5;
-    private final Rectangle[] rectangles = new Rectangle[3];
-    public Rectangle hitbox;
 
-    public Bullet(int starting_x, int starting_y, int size_input)
+    public Bullet(int x, int y, int size_input)
     {
-        x = starting_x;
-        y = starting_y;
-        size = size_input;
+        super(x, y);
+        rectangles = new Rectangle[3];
 
+        size = size_input;
         rectangles[0] = new Rectangle(x, y+(2*size), 1*size, 2*size, "BLACK", 9);
         rectangles[1] = new Rectangle(x+(1*size), y+(1*size), 1*size, 4*size, "BLACK", 9);
         rectangles[2] = new Rectangle(x+(2*size), y, 6*size, 6*size, "BLACK", 9);
@@ -35,34 +31,5 @@ public class Bullet {
             }
         }
         return false;
-    }
-
-    public void addTo(GameArena gameArena)
-    {
-        for (Rectangle rectangle : rectangles)
-        {
-            gameArena.addRectangle(rectangle);
-        }
-    }
-
-    public void move(double dx, double dy)
-    {
-        for (Rectangle rectangle : rectangles)
-        {
-            rectangle.setXPosition(rectangle.getXPosition()+dx);
-            rectangle.setYPosition(rectangle.getYPosition()+dy);
-        }
-        x += dx;
-        y += dy;
-    }
-
-    public void setPos(double new_x, double new_y)
-    {
-        for (Rectangle rectangle : rectangles) {
-            rectangle.setXPosition((rectangle.getXPosition()-x)+new_x);
-            rectangle.setYPosition((rectangle.getYPosition()-y)+new_y);
-        }
-        x = new_x;
-        y = new_y;
     }
 }
