4c9611d8deed4b1188b7cb2443ee7be1cc51177d
Toby <tobyeclark@gmail.com>
2024-03-03 08:45:21 +0000

    Renamed size and added a const for window size

--- Battery.java
+++ Battery.java
@@ -1,27 +1,28 @@
 import java.util.Random;
 
 public class Battery extends GameObject {
+    private static final int WINDOW_SIZE = 1000;
     private final Random rand = new Random();
 
-    public Battery(double size)
+    public Battery(double scaleMultiplier)
     {
-        super(0, 0, size);
-        x = rand.nextInt(940);
-        y = rand.nextInt(940);
+        super(0, 0, scaleMultiplier);
+        x = rand.nextDouble(WINDOW_SIZE - (scaleMultiplier*6));
+        y = rand.nextDouble(WINDOW_SIZE - (scaleMultiplier*6));
 
         rectangles = new Rectangle[3];
 
-        rectangles[0] = new Rectangle(x, y, 2*size, 5*size, "WHITE", 0);
+        rectangles[0] = new Rectangle(x, y, 2*scaleMultiplier, 5*scaleMultiplier, "WHITE", 0);
         hitbox =  rectangles[0];
 
-        rectangles[1] = new Rectangle(x, y, 2*size, 2*size, "ORANGE", 9);
-        rectangles[2] = new Rectangle(x, y+(2*size), 2*size, 3*size, "BLACK", 9);
+        rectangles[1] = new Rectangle(x, y, 2*scaleMultiplier, 2*scaleMultiplier, "ORANGE", 9);
+        rectangles[2] = new Rectangle(x, y+(2*scaleMultiplier), 2*scaleMultiplier, 3*scaleMultiplier, "BLACK", 9);
     }
 
     public void captured()
     {
-        int new_x = rand.nextInt(440);
-        int new_y = rand.nextInt(440) + 50;
+        double new_x = rand.nextDouble(WINDOW_SIZE - (scaleMultiplier*6));
+        double new_y = rand.nextDouble(WINDOW_SIZE - (scaleMultiplier*6)) + 50;
 
         // ensures it spawns away from where the last one was
         if(x < 500)
